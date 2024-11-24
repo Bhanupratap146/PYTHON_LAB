995495d6b95fc4b5896dcfec876efca48451a455
Bhanupratap146 <BHANU.23FE10CSE00220@muj.manipal.edu>
2024-11-24 23:44:01 +0530

    Password.py

--- Python_Lab6_cont/Password.py
+++ Python_Lab6_cont/Password.py
@@ -4,7 +4,7 @@ class PasswordManager:
     def set_password(self, password):
       self.__password = password
     def get_password(self):
-    return self.__password
+      return self.__password
 password_manager = PasswordManager()
 password_manager.set_password("mySecretPassword123")
 print("Password is:", password_manager.get_password())
